d4a42478aa42cceabd8d55ba7cdb1f421e2600f3
msiarez <msiarez@terpmail.umd.edu>
2026-02-20 21:55:55 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -7,4 +7,4 @@ for i in range(10000):
   if roll1 == roll2:
     doubles += 1
 percentage = (doubles / 10000) * 100
-print("Percentage of doubles out of 10000 rolls is:{0:0.2f}%".format(percentage)
+print("Percentage of doubles out of 10000 rolls is:{0:0.2f}%".format(percentage))
