6df3079e2def6139a44d3f8ac869bf30695cf5c4
msiarez <msiarez@terpmail.umd.edu>
2026-02-20 21:53:45 -0500

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,10 @@
+#Question 3
+import random
+doubles = 1
+for i in range(10000):
+  roll1 = random.randint(1,6)
+  roll2 = random.randint(1,6)
+      if roll1 == roll2:
+        doubles += 1
+percentage = (doubles / 10000) * 100
+print("Percentage of doubles out of 10000 rolls is:{0:0.2f}%".format(percentage)
